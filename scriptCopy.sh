9fbdbec3d5d5a875285c55fb40ff0ed8766b6492
RichaKulkarni <RichaKulkarni@users.noreply.github.com>
2018-04-23 08:59:48 -0700

    Update scriptCopy.sh

--- scriptCopy.sh
+++ scriptCopy.sh
@@ -2,4 +2,3 @@ sudo aws s3 cp s3://latticeengines-dev-emr/scripts/tezUI.sh /scripts/
 sudo aws s3 cp s3://latticeengines-dev-emr/scripts/nodeLabels.sh /scripts/
 sudo aws s3 cp s3://latticeengines-dev-emr/scripts/attachNodeLabels.bash /scripts/
 sudo aws s3 cp s3://latticeengines-dev-emr/scripts/downloadPemSSH.bash /scripts/
-sudo aws s3 cp s3://latticeengines-dev-emr/scripts/cronScheduler.bash /scripts/
