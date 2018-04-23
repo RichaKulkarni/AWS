d41b71abc96332402f43b497d611512ea85d2ed6
RichaKulkarni <RichaKulkarni@users.noreply.github.com>
2018-04-23 08:47:14 -0700

    scriptCopy.sh

---
+++ scriptCopy.sh
@@ -0,0 +1,5 @@
+sudo aws s3 cp s3://latticeengines-dev-emr/scripts/tezUI.sh /scripts/
+sudo aws s3 cp s3://latticeengines-dev-emr/scripts/nodeLabels.sh /scripts/
+sudo aws s3 cp s3://latticeengines-dev-emr/scripts/attachNodeLabels.bash /scripts/
+sudo aws s3 cp s3://latticeengines-dev-emr/scripts/downloadPemSSH.bash /scripts/
+sudo aws s3 cp s3://latticeengines-dev-emr/scripts/cronScheduler.bash /scripts/
